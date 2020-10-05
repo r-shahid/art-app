9b4035600150ddc392ccbde57a94e7ec71d22efe
Riana <rianashahid@gmail.com>
2020-10-05 17:28:46 -0400

    random component started

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './Home'
-import Random from './Random'
+import Random from './Random/Random'
 
 
 const Main = () => {
