0f0345b01e1becf288b11e6d3749703b5f6ed4bb
Riana <rianashahid@gmail.com>
2020-10-05 15:19:27 -0400

    almost done - add buttons

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import rebecca from './Images/abduction-of-rebecca.jpg'
+import renoir from './Images/stilllife-renoir.jpg'
+import vincent from './Images/selfportrait-vangogh.jpg'
+
 
 const Home = () => {
 
-    return(
+    return (
         <div className="home-page">
-        <h1>this is the homepage</h1>
+            <div className='main'>
+                <div className='hero-img'>
+                    <div><p className='overlaid-text'>Explore public domain art from the Metropolitan Museum of Art's Collection</p></div>
+                    {/* <img src={rebecca} alt='rebecca' /> */}
+
+                </div>
+                <div className="main-bottom">
+                    <div className='random-img'>
+                        <div><p className='overlaid-text'>Show me a random piece of art</p></div>
+                    </div>
+                    <div className='search-img'>
+                        {/* <img className='vincent' src={vincent} alt='self portrait with a straw har - vincent van gogh' /> */}
+                        <div><p className='overlaid-text'>I know what I'm looking for...</p></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
